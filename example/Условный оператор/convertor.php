cb0b9a43daa13b9c3ef3e5398f18f5c814c83056
roevevgen <roevevgen7@gmail.com>
2024-01-27 21:44:46 +0200

    Условный оператор

---
+++ example/Условный оператор/convertor.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Конвертор</title>
+</head>
+<body>
+<h1>Преобразование из дюймов в сантиметры</h1>
+<form action="convertor3.php" method="get">
+    <p>Величина в дюймах:
+        <input type="number" name="inches" size="10"></p>
+    <p><input type="submit" value="Преобразовать"></p>
+</form>
+<?php
+    if (isset($_Get['inches'])) {
+        $ins = $_Get['inches'];
+        $cents = round($ins * 2.54);
+        echo '<p>', $ins, ' дюймов = ', $cents, ' сантиметров.</p>';
+    }
+
+?>
+</body>
+</html>
