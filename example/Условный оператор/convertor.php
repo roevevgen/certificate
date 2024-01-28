9fd32d9741a72d46aabf4fb5cb1b952ac366e2aa
roevevgen <roevevgen7@gmail.com>
2024-01-28 16:32:54 +0200

    convertor

--- example/Условный оператор/convertor.php
+++ example/Условный оператор/convertor.php
@@ -6,16 +6,29 @@
 </head>
 <body>
 <h1>Преобразование из дюймов в сантиметры</h1>
-<form action="convertor.php" method="get">
+<form action="convertor.php" method="post">
     <p>Величина в дюймах:
-        <input type="number" name="inches" size="10"></p>
+        <input type="text" name="inches" size="10"></p>
     <p><input type="submit" value="Преобразовать"></p>
 </form>
 <?php
-    if (isset($_GET['inches'])) {
-        $ins = $_GET['inches'];
-        $cents = round($ins * 2.54);
-        echo '<p>', $ins, ' дюймов = ', $cents, ' сантиметров.</p>';
+    if (isset($_POST['inches'])) {
+
+        // Заменяем точку на запятую
+
+        $ins = str_replace(".", ",", $_POST['inches']);
+
+        // Преобразуем строку в число
+
+        $ins = (double)$ins;
+        if ($ins > 0) {
+            $cents = round($ins * 2.54);
+            echo '<p>', $ins, ' дюймов = ', $cents, ' сантиметров.</p>';
+        } else {
+            echo 'Вепичина в дюймах должна быть больше нуля. ';
+        }
+
+
     }
 
 ?>
