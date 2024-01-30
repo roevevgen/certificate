98e3717ab698c8a0365babee12466f39eef89f6f
roevevgen <roevevgen7@gmail.com>
2024-01-31 00:10:12 +0200

    convertor

--- example/Условный оператор/convertor.php
+++ example/Условный оператор/convertor.php
@@ -1,5 +1,5 @@
 <!doctype html>
-<html>
+<html lang="en">
 <head>
     <meta charset="UTF-8">
     <title>Конвертор</title>
@@ -7,8 +7,8 @@
 <body>
 <h1>Преобразование из дюймов в сантиметры</h1>
 <form action="convertor.php" method="post">
-    <p>Величина в дюймах:
-        <input type="text" name="inches" size="10"></p>
+    <label for="inches">Величина в дюймах:</label>
+    <input type="text" name="inches" id="inches" size="10">
     <p><input type="submit" value="Преобразовать"></p>
 </form>
 <?php
@@ -23,12 +23,23 @@
         $ins = (double)$ins;
         if ($ins > 0) {
             $cents = round($ins * 2.54);
-            echo '<p>', $ins, ' дюймов = ', $cents, ' сантиметров.</p>';
-        } else {
-            echo 'Вепичина в дюймах должна быть больше нуля. ';
+            $is = (string)$ins;
+            if (str_contains($is, '.') || (strlen($is) > 1 && $is[-2] == '1'))
+
+                $ins_ending = 'ов';
+            else {
+                $is = $is[-1];
+                if ($is == 1)
+                    $ins_ending = '';
+                else if ($is >= 2 && $is <= 4)
+                    $ins_ending = 'а';
+                else
+                    $ins_ending = 'ов';
+            }
         }
-
-
+        echo "<p>{$ins} дюйм{$ins_ending} = {$cents} сантиметров.</p>";
+    } else {
+        echo '<p>Величина в дюймах должна быть больше нуля.</p>';
     }
 
 ?>
